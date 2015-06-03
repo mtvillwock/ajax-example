bae5fc0b32940edd1bc7a4a1917ba63ae5d6d849
Matthew Villwock <mtvillwock@gmail.com>
2015-06-02 19:40:35 -0700

    add p statements for debugging, indent params for note creation

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -4,9 +4,12 @@ end
 
 post '/notes' do
   p params
-  note = Note.new(title: params[:title], content: params[:content])
+  note = Note.new(title: params[:title],
+                  content: params[:content])
+  p note
   content_type :json
   if note.save!
+    p note
     note.to_json
   else
     {error: "post failed to save"}.to_json
