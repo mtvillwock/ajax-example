d9ee8244630b4e696c0ec20ee919faea85d51c62
Matthew Villwock <mtvillwock@gmail.com>
2015-06-02 18:16:24 -0700

    add form and post route

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -2,6 +2,16 @@ get '/' do
   erb :index
 end
 
+post '/notes' do
+  post = Post.new(title: params[:title], content: params[:content])
+  content_type :json
+  if post.save!
+    post.to_json
+  else
+    {error: "post failed to save"}.to_json
+  end
+end
+
 get '/login' do
 
 end
